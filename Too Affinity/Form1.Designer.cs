4585735f94d6b5286e5ddcdb6216dc4a6fecdb33
Ryan Bucshon <busheezy@gmail.com>
2022-06-25 11:34:56 -0500

    wooo - Jace

--- Too Affinity/Form1.Designer.cs
+++ Too Affinity/Form1.Designer.cs
@@ -31,6 +31,13 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.startWithWindowsCb = new System.Windows.Forms.CheckBox();
+            this.startMinimizedCb = new System.Windows.Forms.CheckBox();
+            this.disableFirstCoreCb = new System.Windows.Forms.CheckBox();
+            this.disableHtCb = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -39,23 +46,97 @@
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 139);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(384, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // startWithWindowsCb
+            // 
+            this.startWithWindowsCb.AutoSize = true;
+            this.startWithWindowsCb.Location = new System.Drawing.Point(12, 12);
+            this.startWithWindowsCb.Name = "startWithWindowsCb";
+            this.startWithWindowsCb.Size = new System.Drawing.Size(130, 19);
+            this.startWithWindowsCb.TabIndex = 1;
+            this.startWithWindowsCb.Text = "Start With Windows";
+            this.startWithWindowsCb.UseVisualStyleBackColor = true;
+            this.startWithWindowsCb.CheckedChanged += new System.EventHandler(this.startWithWindowsCb_CheckedChanged);
+            // 
+            // startMinimizedCb
+            // 
+            this.startMinimizedCb.AutoSize = true;
+            this.startMinimizedCb.Location = new System.Drawing.Point(12, 37);
+            this.startMinimizedCb.Name = "startMinimizedCb";
+            this.startMinimizedCb.Size = new System.Drawing.Size(109, 19);
+            this.startMinimizedCb.TabIndex = 2;
+            this.startMinimizedCb.Text = "Start Minimized";
+            this.startMinimizedCb.UseVisualStyleBackColor = true;
+            this.startMinimizedCb.CheckedChanged += new System.EventHandler(this.startMinimizedCb_CheckedChanged);
+            // 
+            // disableFirstCoreCb
+            // 
+            this.disableFirstCoreCb.AutoSize = true;
+            this.disableFirstCoreCb.Location = new System.Drawing.Point(12, 62);
+            this.disableFirstCoreCb.Name = "disableFirstCoreCb";
+            this.disableFirstCoreCb.Size = new System.Drawing.Size(117, 19);
+            this.disableFirstCoreCb.TabIndex = 3;
+            this.disableFirstCoreCb.Text = "Disable First Core";
+            this.disableFirstCoreCb.UseVisualStyleBackColor = true;
+            this.disableFirstCoreCb.CheckedChanged += new System.EventHandler(this.disableFirstCoreCb_CheckedChanged);
+            // 
+            // disableHtCb
+            // 
+            this.disableHtCb.AutoSize = true;
+            this.disableHtCb.Location = new System.Drawing.Point(12, 87);
+            this.disableHtCb.Name = "disableHtCb";
+            this.disableHtCb.Size = new System.Drawing.Size(138, 19);
+            this.disableHtCb.TabIndex = 4;
+            this.disableHtCb.Text = "Disable Hyperthreads";
+            this.disableHtCb.UseVisualStyleBackColor = true;
+            this.disableHtCb.CheckedChanged += new System.EventHandler(this.disableHtCb_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.disableHtCb);
+            this.Controls.Add(this.disableFirstCoreCb);
+            this.Controls.Add(this.startMinimizedCb);
+            this.Controls.Add(this.startWithWindowsCb);
+            this.Controls.Add(this.statusStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Too Affinity";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private NotifyIcon notifyIcon1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private CheckBox startWithWindowsCb;
+        private CheckBox startMinimizedCb;
+        private CheckBox disableFirstCoreCb;
+        private CheckBox disableHtCb;
     }
 }
--- Too Affinity/Form1.cs
+++ Too Affinity/Form1.cs
@@ -1,7 +1,11 @@
+using Microsoft.Win32;
+
 namespace Too_Affinity
 {
     public partial class Form1 : Form
     {
+        string appName = "Too Affinity";
+
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +13,23 @@ namespace Too_Affinity
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            RegistryKey? rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
 
+            if (rk == null)
+            {
+                Application.Exit();
+                return;
+            }
+
+            var existingRegistryValue = rk.GetValue(appName);
+            if (existingRegistryValue != null)
+            {
+                startWithWindowsCb.Checked = true;
+            }
+
+            startMinimizedCb.Checked = Properties.Settings.Default.startMinimized;
+            disableFirstCoreCb.Checked = Properties.Settings.Default.disableFirstCore;
+            disableHtCb.Checked = Properties.Settings.Default.disableHt;
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -26,6 +46,53 @@ namespace Too_Affinity
             Show();
             this.WindowState = FormWindowState.Normal;
             notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.startMinimized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.Hide();
+            }
+        }
+
+        private void startWithWindowsCb_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey? rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (rk == null)
+            {
+                Application.Exit();
+                return;
+            }
+
+            if (startWithWindowsCb.Checked)
+            {
+                rk.SetValue(appName, Application.ExecutablePath);
+            }
+            else
+            {
+                rk.DeleteValue(appName, false);
+            }
+        }
+
+        private void startMinimizedCb_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.startMinimized = startMinimizedCb.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void disableFirstCoreCb_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.disableFirstCore = disableFirstCoreCb.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void disableHtCb_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.disableHt = disableHtCb.Checked;
+            Properties.Settings.Default.Save();
         }
     }
 }
